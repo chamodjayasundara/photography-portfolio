46317193e37dfbd1ff6d840c830fe6e2a2ef7812
Chamod Jayasundara <chamodjayamal@gmail.com>
2025-11-24 19:07:07 +0530

    praneeth testimonial

--- pages/about.js
+++ pages/about.js
@@ -27,13 +27,13 @@ const services = [
 ];
 
 const testimonials = [
-  /*{
-    name: "Luca Poloni",
-    designation: "Co-Owner",
-    company: "Ellerton Bungalow",
-    text: "It was an absolute pleasure working with Chamod Jayasundara during our recent property shoot at Victoria Golf Resort. He beautifully captured the essence of the resort, from our rooms and restaurant to the golf course and clubhouse - with a truly artistic eye. The final images were nothing short of stunning, perfectly reflecting the natural charm and elegance of the property. Chamod was incredibly professional, flexible, and easy to work with, allowing our daily operations to continue seamlessly while he worked around the resort. We're truly grateful for his effort and creativity, and look forward to collaborating again in the future!",
+  {
+    name: "Praneeth Madusanka",
+    designation: "Social Media Specialist",
+    company: "Ceylon Hotels Corporation",
+    text: "Chamod is truly one of the best photographers I have had the opportunity to work with. His work ethic, professionalism, and consistency make every collaboration effortless. Whether it’s architectural photography or any other project, he is someone I can confidently recommend without hesitation. I’m incredibly proud to see the growth he brings to each assignment, elevating his craft with every shoot. Looking forward to many more projects together. Wishing him all the very best!",
     image: "/images/testimonials/client1.jpg"
-  }, */
+  }, 
   {
     name: "Fadhil Fazil",
     designation: "Marketing Executive",
